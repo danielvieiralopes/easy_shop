4c67cbbe5b1feacbb7592e9c1da07ee04d810df4
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-08-04 17:08:00 -0300

    tamanhos produto

--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -9,11 +9,15 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final primaryColor = Theme.of(context).primaryColor;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
         centerTitle: true,
       ),
+      backgroundColor: Colors.white,
       body: ListView(
         children: <Widget>[
           AspectRatio(
@@ -23,9 +27,55 @@ class ProductScreen extends StatelessWidget {
                 return NetworkImage(url);
               }).toList(),
               dotSize: 4,
+              dotSpacing: 15,
               dotBgColor: Colors.transparent,
               dotColor: Theme.of(context).primaryColor,
               autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  product.name,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    'A partir de',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                Text(
+                  'R\$ 19.99',
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ), 
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Descrição',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  ),
+                 Text(
+                   product.description,
+                   style: const TextStyle(
+                     fontSize: 16
+                   ),
+                 ),                
+              ],
             ),
           )
         ],
